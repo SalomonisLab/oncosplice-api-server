9fbd9a4198197b838b312c3cde087512fb38f095
stuarthay1991 <haysb91@gmail.com>
2022-09-13 14:32:51 -0400

    renamed a few variables

--- app/controllers/getInteractiveFilter.js
+++ app/controllers/getInteractiveFilter.js
@@ -6,41 +6,41 @@ async function getInteractiveFilter(req, res, next){
 		try{
 			var outputObject = {};
 			var cancerType = req.body.data.cancerType;
-			var postName = req.body.data.name;
+			var selectedFilterName = req.body.data.name;
 			var queryHelperMap = databaseQueryHelper(cancerType);
 			var set = [];
-			var color_push = {};
-			var returned_result = {};
-			var i_set = 0;
-			var clinicalMetaDataQuery = "SELECT ".concat(postName.toLowerCase()).concat(", uid ").concat(queryHelperMap["META"]["QUERY"]);
+			var colorMatchArray = {};
+			var outputArray = {};
+			var iSet = 0;
+			var clinicalMetaDataQuery = "SELECT ".concat(selectedFilterName.toLowerCase()).concat(", uid ").concat(queryHelperMap["META"]["QUERY"]);
 			var clinicalMetaDataResult = await dbCredentials.query(clinicalMetaDataQuery);
 
 			clinicalMetaDataResult.rows.forEach(row => {
 				var str_edit = (row['uid'].replace(/\.|\-/g, "_")).toLowerCase();
-				returned_result[str_edit] = row[postName.toLowerCase()];
-				var found_flag = 0;
+				outputArray[str_edit] = row[selectedFilterName.toLowerCase()];
+				var foundFlag = 0;
 			    for(let k = 0; k < set.length; k++)
 			    {
-			    	if(set[k] ==  row[postName.toLowerCase()])
+			    	if(set[k] ==  row[selectedFilterName.toLowerCase()])
 			    	{
-			    		found_flag = 1;
+			    		foundFlag = 1;
 			    	}
 			    }
-			    if(found_flag == 0)
+			    if(foundFlag == 0)
 			    {
-			    	set[i_set] = row[postName.toLowerCase()];
-			    	i_set += 1;
+			    	set[iSet] = row[selectedFilterName.toLowerCase()];
+			    	iSet += 1;
 			    }
 			})
 
 			for(let k = 0; k < set.length; k++)
 			{
-				color_push[set[k]] = k;
+				colorMatchArray[set[k]] = k;
 			}
 
 			outputObject["set"] = set;
-			outputObject["out"] = returned_result;
-			outputObject["color"] = color_push;
+			outputObject["out"] = outputArray;
+			outputObject["color"] = colorMatchArray;
 			res.send(outputObject);
 		}
 		catch(error){
